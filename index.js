cc2fd9405eb732a6243ebd9a2fdc6ec59b4a4909
amenov <amenov.abdulsalam@gmail.com>
2023-11-23 20:16:27 +0600

    add middleware: respond

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   getModels: require('./lib/get-models'),
-  printServerInfo: require('./lib/print-server-info')
+  printServerInfo: require('./lib/print-server-info'),
+  middleware: {
+    respond: require('./lib/middleware/respond')
+  }
 }
